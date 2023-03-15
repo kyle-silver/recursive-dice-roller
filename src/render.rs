fb9c7df5403655a9abf656eb6a0ff2f16a4fc98e
Kyle Silver <kyleAsilver@gmail.com>
2023-03-14 21:28:06 -0400

    better render

--- src/render.rs
+++ src/render.rs
@@ -1,21 +1,35 @@
-use std::slice::Iter;
-
-use itertools::Itertools;
-
-use crate::eval::{KeptRule, Value};
+use crate::eval::{KeptRule, Operation, Value};
 
 #[derive(Debug, Default)]
 struct RenderNode {
     expression: String,
-    output: String,
+    output: Option<String>,
     children: Vec<RenderNode>,
 }
 
 impl RenderNode {
-    fn create(value: &Value) -> Option<Self> {
+    fn create(value: &Value, parent_op: Option<&Operation>, first: bool) -> Option<Self> {
         match value {
             Value::Unit => None,
-            Value::Const(_) => None,
+            Value::Const(c) => match parent_op {
+                Some(op) => {
+                    let operator = match op {
+                        Operation::Add => '+',
+                        Operation::Sub => '-',
+                        Operation::Mul => '\u{00D7}',
+                    };
+                    Some(RenderNode {
+                        expression: if first {
+                            format!("({c})")
+                        } else {
+                            format!("({operator}{c})")
+                        },
+                        output: None,
+                        children: Vec::new(),
+                    })
+                }
+                None => None,
+            },
             Value::Rolled(rolled) => {
                 let components: [&Value; 3] = [
                     rolled.dice.as_ref(),
@@ -24,40 +38,44 @@ impl RenderNode {
                 ];
                 let children: Vec<RenderNode> = components
                     .into_iter()
-                    .filter_map(|v| RenderNode::create(v))
+                    .enumerate()
+                    .filter_map(|(i, v)| RenderNode::create(v, None, i == 0))
                     .collect();
                 let output = rolled.val();
                 match &rolled.kept.keep {
                     KeptRule::All => Some(RenderNode {
                         expression: format!("Rolling {value}"),
-                        output: format!("{:?} => {output}", rolled.kept.highest),
+                        output: Some(format!("{:?} => {output}", rolled.kept.highest)),
                         children,
                     }),
                     _ => Some(RenderNode {
                         expression: format!("Rolling {value}"),
-                        output: format!(
+                        output: Some(format!(
                             "({:?} | {:?}) => {output}",
                             rolled.kept.highest, rolled.kept.lowest
-                        ),
+                        )),
                         children,
                     }),
                 }
             }
-            Value::Op { values, .. } => {
+            Value::Op { op, values, .. } => {
                 let children = values
                     .iter()
                     .flat_map(|v| {
-                        let x: Box<dyn Iterator<Item = &Value>> = match v {
-                            Value::Op { values, .. } => Box::new(values.iter()),
-                            x => Box::new(std::iter::once(x)),
+                        let x: Box<dyn Iterator<Item = (&Value, Operation)>> = match v {
+                            Value::Op {
+                                op: sub_op, values, ..
+                            } => Box::new(values.iter().map(|v| (v, sub_op.clone()))),
+                            x => Box::new(std::iter::once((x, op.clone()))),
                         };
                         x
                     })
-                    .filter_map(RenderNode::create)
+                    .enumerate()
+                    .filter_map(|(i, (v, operator))| RenderNode::create(v, Some(&operator), i == 0))
                     .collect();
                 Some(RenderNode {
                     expression: format!("Evaluating {value}"),
-                    output: format!("{}", value.value()),
+                    output: Some(format!("{}", value.value())),
                     children,
                 })
             }
@@ -66,7 +84,7 @@ impl RenderNode {
 }
 
 pub fn no_color(value: &Value) {
-    let render: Option<RenderNode> = RenderNode::create(value);
+    let render: Option<RenderNode> = RenderNode::create(value, None, true);
     match render {
         Some(render) => draw(&render, 0),
         None => println!("{}", value.value()),
@@ -82,18 +100,30 @@ fn draw(node: &RenderNode, depth: i32) {
     if depth == 0 {
         println!("{}", node.expression);
     } else {
-        println!("{indent}+---{}", node.expression)
+        println!("{indent}+-- {}", node.expression)
     }
     for child in &node.children {
         draw(child, depth + 1);
     }
     if node.children.is_empty() {
-        println!("{indent}|   {}", node.output);
+        if depth == 0 {
+            if let Some(output) = &node.output {
+                println!("{indent}{}", output);
+            }
+        } else {
+            if let Some(output) = &node.output {
+                println!("{indent}|   {}", output);
+            }
+        }
         return;
     }
     if depth == 0 {
-        println!("{}", node.output);
+        if let Some(output) = &node.output {
+            println!("{}", output);
+        }
     } else {
-        println!("{indent}|   {}", node.output);
+        if let Some(output) = &node.output {
+            println!("{indent}|   {}", output);
+        }
     }
 }
